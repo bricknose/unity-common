8940fdcdb24d58193d54cd82e312200301b8357c
bricknose <me@bricknose.com>
2019-08-22 21:11:04 -0500

    Decompose Billboard animation into AnimatedBillboard script.

---
+++ source/Unity.Common/Unity.Common.Pseudo3D/AnimatedBillboard.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+
+namespace Unity.Common.Pseudo3D
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    [RequireComponent(typeof(Animator))]
+    [RequireComponent(typeof(Billboard))]
+    public class AnimatedBillboard : MonoBehaviour
+    {
+        private Animator _animator;
+        private Billboard _billboard;
+        private float _maxMirrorAngle;
+        private float _minMirrorAngle;
+        private SpriteRenderer _spriteRenderer;
+
+        public int Directions = 8;
+        public bool MirrorLeft = true;
+        public string AnimatorAngleVariable = "ViewAngle";
+
+        public void Start()
+        {
+            GetDependencies();
+            InitializeBillboard();
+        }
+
+        public void OnDestroy()
+        {
+            CleanUpDependencies();
+        }
+
+        private void GetDependencies()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            _animator = GetComponent<Animator>();
+            _billboard = GetComponent<Billboard>();
+
+            _billboard.BillboardAngleUpdatedEvent += AngleUpdatedSpriteMirroring;
+            _billboard.BillboardAngleUpdatedEvent += AngleUpdatedAnimatorViewAngle;
+        }
+
+        private void InitializeBillboard()
+        {
+            if (Directions <= 0)
+            {
+                Directions = 1;
+            }
+
+            _minMirrorAngle = 360f / 2 / Directions;
+            _maxMirrorAngle = 180f - _minMirrorAngle;
+        }
+
+        private void CleanUpDependencies()
+        {
+            _billboard.BillboardAngleUpdatedEvent -= AngleUpdatedSpriteMirroring;
+            _billboard.BillboardAngleUpdatedEvent -= AngleUpdatedAnimatorViewAngle;
+        }
+
+        private void AngleUpdatedSpriteMirroring(float billboardAngle)
+        {
+            if (MirrorLeft)
+            {
+                _spriteRenderer.flipX = !(billboardAngle >= _minMirrorAngle &&
+                                          billboardAngle <= _maxMirrorAngle);
+            }
+        }
+
+        private void AngleUpdatedAnimatorViewAngle(float billboardAngle)
+        {
+            _animator.SetFloat(AnimatorAngleVariable, billboardAngle);
+        }
+    }
+}
--- source/Unity.Common/Unity.Common.Pseudo3D/Billboard.cs
+++ source/Unity.Common/Unity.Common.Pseudo3D/Billboard.cs
@@ -3,43 +3,26 @@ using UnityEngine;
 
 namespace Unity.Common.Pseudo3D
 {
-    [RequireComponent(typeof(Animator))]
-    [RequireComponent(typeof(SpriteRenderer))]
     public class Billboard : MonoBehaviour
     {
-        private Animator _animator;
-        private float _maxMirrorAngle;
-        private float _minMirrorAngle;
-        private SpriteRenderer _spriteRenderer;
+        public delegate void BillboardAngleUpdated(float newBillboardAngle);
 
-        public int Directions = 8;
-        public bool MirrorLeft = true;
+        public event BillboardAngleUpdated BillboardAngleUpdatedEvent;
+
         public Camera MainCamera;
-        public string AnimatorAngleVariable = "ViewAngle";
-
-        public void Start()
-        {
-            GetDependencies();
-            InitializeBillboard();
-        }
 
         public void Awake()
         {
             UpdateMainCamera();
         }
 
-        public void Update()
+        public virtual void Update()
         {
             var viewDirection = GetBillboardTargetDirection(MainCamera.transform);
             FaceBillboardTowardTarget(viewDirection);
 
             var billboardAngle = GetBillboardViewAngle();
-            UpdateAnimatorViewAngle(billboardAngle);
-
-            if (MirrorLeft)
-            {
-                UpdateSpriteMirroring(billboardAngle);
-            }
+            BillboardAngleUpdatedEvent?.Invoke(billboardAngle);
         }
 
         public void OnDrawGizmos()
@@ -53,40 +36,12 @@ namespace Unity.Common.Pseudo3D
             DrawBillboardDirectionRay();
         }
 
-        private void GetDependencies()
-        {
-            _animator = GetComponent<Animator>();
-            _spriteRenderer = GetComponent<SpriteRenderer>();
-        }
-
-        private void InitializeBillboard()
-        {
-            if (Directions <= 0)
-            {
-                Directions = 1;
-            }
-
-            _minMirrorAngle = 360f / 2 / Directions;
-            _maxMirrorAngle = 180f - _minMirrorAngle;
-        }
-
         private void UpdateMainCamera()
         {
             if (MainCamera == null)
             {
                 MainCamera = Camera.main;
             }
-        }
-
-        private void UpdateSpriteMirroring(float billboardAngle)
-        {
-            _spriteRenderer.flipX = !(billboardAngle >= _minMirrorAngle &&
-                                      billboardAngle <= _maxMirrorAngle);
-        }
-
-        private void UpdateAnimatorViewAngle(float billboardAngle)
-        {
-            _animator.SetFloat(AnimatorAngleVariable, billboardAngle);
         }
 
         private float GetBillboardViewAngle()
